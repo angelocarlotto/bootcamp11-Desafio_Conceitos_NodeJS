df950ae4f9f1ed8e42f948ada8b7d296edfae9ee
Angelo Carlotto <angelocarlotto@Angelos-MacBook-Pro.local>
2020-04-13 10:18:29 -0400

    implementação dos metodos solicitados no desafio

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+ const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,57 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {url,title,techs}=request.body;
+  const newObj={url,title,techs};
+  newObj.id=uuid();
+  newObj.likes=0;
+  repositories.push(newObj);
+  return response.json(newObj);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id}=request.params;
+  const {title,url,techs}=request.body;
+  const indexToUpdate=repositories.findIndex(e=>e.id===id);
+  if(indexToUpdate>=0){
+    const projectUpdated={
+      id:repositories[indexToUpdate].id,
+      title:title,
+      url:url,
+      techs:techs,
+      likes:repositories[indexToUpdate].likes
+    };
+    repositories[indexToUpdate]=projectUpdated;
+    return response.json(projectUpdated);
+  }
+
+  return response.status(400).json({error:`repository not found`});
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id}=request.params;
+  const indexToDelete=repositories.findIndex(e=>e.id===id);
+
+  if(indexToDelete>=0){
+      repositories.splice(indexToDelete,1);
+      return response.status(204).send();
+  }
+
+  return response.status(400).json({error:`repository not found`});
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id}=request.params;
+  const indexToUpdate=repositories.findIndex(e=>e.id===id);
+  if(indexToUpdate>=0){
+    repositories[indexToUpdate].likes=repositories[indexToUpdate].likes+1;
+    return response.json(repositories[indexToUpdate]);
+  }
+  return response.status(400).json({error:`repository not found`});
 });
 
 module.exports = app;
